20765aea2ac1e5e66d54892b5213f5de0968fcb0
artdev <artdevs23@gmail.com>
2020-03-04 05:39:50 +0300

    начальная структура проекта

---
+++ task-tracker-app/src/test/java/ru/geekbrains/TaskTrackerApplicationTests.java
@@ -0,0 +1,17 @@
+package ru.geekbrains;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+//@SpringBootTest
+class TaskTrackerApplicationTests
+{
+
+  @Test
+  void contextLoads()
+  {
+  }
+
+}
