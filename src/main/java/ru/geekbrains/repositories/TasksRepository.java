c4abbde347cb8883214173b1b65e2a3d3bc64852
enjsite <enjyweb@gmail.com>
2020-04-11 02:49:49 +0300

    order tasks by id

--- src/main/java/ru/geekbrains/repositories/TasksRepository.java
+++ src/main/java/ru/geekbrains/repositories/TasksRepository.java
@@ -11,12 +11,12 @@ import java.util.List;
 @Repository
 public interface TasksRepository extends JpaRepository<Task, Long> {
 
-    @Query("select t from Task t where t.manager_id = :manager_id")
+    @Query("select t from Task t where t.manager_id = :manager_id order by id")
     List<Task> findByManager_id(@Param("manager_id") Long id);
 
-    @Query("select t from Task t where t.employer_id = :employer_id")
+    @Query("select t from Task t where t.employer_id = :employer_id order by id")
     List<Task> findByEmployer_id(@Param("employer_id") Long id);
 
-    @Query("select t from Task t where t.manager_id = :user_id or t.employer_id = :user_id")
+    @Query("select t from Task t where t.manager_id = :user_id or t.employer_id = :user_id order by id")
     List<Task> findByManager_idAndEmployer_id(@Param("user_id") Long id);
 }
