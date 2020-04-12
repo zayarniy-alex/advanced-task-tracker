39549f5c070aeb091a550a1aadaad0b43da7dae2
enjsite <enjyweb@gmail.com>
2020-04-12 12:23:01 +0300

    fix filtering by users

--- src/main/java/ru/geekbrains/utils/TaskFilter.java
+++ src/main/java/ru/geekbrains/utils/TaskFilter.java
@@ -18,18 +18,16 @@ public class TaskFilter {
         this.spec = Specification.where(null);
         this.filterDefinition = new StringBuilder();
 
-        // как объединить эти два условия - чтобы показывать и созданные юзером задачи, и назначенные ему?
-        // сейчас - с условием or получается, что задача ИЛИ соответствует всем условиям фильтра ИЛИ назначена ему
         if (map.containsKey("manager_id") && !map.get("manager_id").isEmpty()
             && map.containsKey("employer_id") && !map.get("employer_id").isEmpty()) {
 
             Long managerId = Long.parseLong(map.get("manager_id"));
-            spec = spec.and(TaskSpecifications.managerIdEquals(managerId));
             filterDefinition.append("&manager_id=").append(managerId);
 
             Long employerId = Long.parseLong(map.get("employer_id"));
-            spec = spec.or(TaskSpecifications.employerIdEquals(employerId));
             filterDefinition.append("&employer_id=").append(employerId);
+
+            spec = spec.and(TaskSpecifications.managerIdEquals(managerId).or(TaskSpecifications.employerIdEquals(employerId)));
         }
 
 //        if (map.containsKey("urgency") && !map.get("urgency").isEmpty()) {
