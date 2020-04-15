452e9918a24874f99a1a57f07a04fe8b1bf46e61
enjsite <enjyweb@gmail.com>
2020-04-16 01:09:27 +0300

    filter code improving

--- src/main/java/ru/geekbrains/utils/TaskFilter.java
+++ src/main/java/ru/geekbrains/utils/TaskFilter.java
@@ -21,60 +21,41 @@ public class TaskFilter {
         this.spec = Specification.where(null);
         this.filterDefinition = new StringBuilder();
 
+        Long employerId = 0L;
+        Long managerId = 0L;
+
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+
+        if (map.containsKey("employer_id") && !map.get("employer_id").isEmpty()) {
+            employerId = Long.parseLong(map.get("employer_id"));
+            filterDefinition.append("&employer_id=").append(employerId);
+        }
+
+        if (map.containsKey("manager_id") && !map.get("manager_id").isEmpty()) {
+            managerId = Long.parseLong(map.get("manager_id"));
+            filterDefinition.append("&manager_id=").append(managerId);
+        }
+
         if (map.containsKey("whois") && !map.get("whois").isEmpty()) {
 
             Integer whois = Integer.parseInt(map.get("whois"));
-            if (whois == 1) {
-                if (map.containsKey("employer_id") && !map.get("employer_id").isEmpty()) {
-                    Long employerId = Long.parseLong(map.get("employer_id"));
-                    filterDefinition.append("&employer_id=").append(employerId);
+            if (whois == 1 && map.containsKey("employer_id") && !map.get("employer_id").isEmpty()) {
                     filterDefinition.append("&whois=").append(whois);
                     spec = spec.and(TaskSpecifications.employerIdEquals(employerId));
-                }
-            } else if (whois == 2) {
-                if (map.containsKey("manager_id") && !map.get("manager_id").isEmpty()) {
-                    Long managerId = Long.parseLong(map.get("manager_id"));
-                    filterDefinition.append("&manager_id=").append(managerId);
+            } else if (whois == 2 && map.containsKey("manager_id") && !map.get("manager_id").isEmpty()) {
                     filterDefinition.append("&whois=").append(whois);
                     spec = spec.and(TaskSpecifications.managerIdEquals(managerId));
-                }
             } else {
                 if (map.containsKey("manager_id") && !map.get("manager_id").isEmpty()
                         && map.containsKey("employer_id") && !map.get("employer_id").isEmpty()) {
-
-                    Long managerId = Long.parseLong(map.get("manager_id"));
-                    filterDefinition.append("&manager_id=").append(managerId);
-
-                    Long employerId = Long.parseLong(map.get("employer_id"));
-                    filterDefinition.append("&employer_id=").append(employerId);
-
                     spec = spec.and(TaskSpecifications.managerIdEquals(managerId).or(TaskSpecifications.employerIdEquals(employerId)));
                 }
             }
         } else {
             if (map.containsKey("manager_id") && !map.get("manager_id").isEmpty()
                     && map.containsKey("employer_id") && !map.get("employer_id").isEmpty()) {
-
-                Long managerId = Long.parseLong(map.get("manager_id"));
-                filterDefinition.append("&manager_id=").append(managerId);
-
-                Long employerId = Long.parseLong(map.get("employer_id"));
-                filterDefinition.append("&employer_id=").append(employerId);
-
                 spec = spec.and(TaskSpecifications.managerIdEquals(managerId).or(TaskSpecifications.employerIdEquals(employerId)));
             }
-        }
-
-        if (map.containsKey("manager_id") && !map.get("manager_id").isEmpty()
-            && map.containsKey("employer_id") && !map.get("employer_id").isEmpty()) {
-
-            Long managerId = Long.parseLong(map.get("manager_id"));
-            filterDefinition.append("&manager_id=").append(managerId);
-
-            Long employerId = Long.parseLong(map.get("employer_id"));
-            filterDefinition.append("&employer_id=").append(employerId);
-
-            spec = spec.and(TaskSpecifications.managerIdEquals(managerId).or(TaskSpecifications.employerIdEquals(employerId)));
         }
 
         if (map.containsKey("urgency") && !map.get("urgency").isEmpty()) {
@@ -94,18 +75,16 @@ public class TaskFilter {
         }
 
         if (map.containsKey("start_time") && !map.get("start_time").isEmpty()) {
-            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
             Date start_time = sdf.parse(map.get("start_time"));
             spec = spec.and(TaskSpecifications.startTimeEquals(start_time));
             filterDefinition.append("&start_time=").append(start_time);
         }
 
         if (map.containsKey("due_time") && !map.get("due_time").isEmpty()) {
-            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
             Date due_time = sdf.parse(map.get("due_time"));
             spec = spec.and(TaskSpecifications.dueTimeEquals(due_time));
             filterDefinition.append("&due_time=").append(due_time);
         }
-
     }
+
 }
