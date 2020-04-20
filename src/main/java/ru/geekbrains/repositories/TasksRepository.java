0f14632bf5f81c08c690839fe18a85533eeeae0c
enjsite <enjyweb@gmail.com>
2020-04-20 16:16:00 +0300

    resource not found and fix active tab in navigation

--- src/main/java/ru/geekbrains/repositories/TasksRepository.java
+++ src/main/java/ru/geekbrains/repositories/TasksRepository.java
@@ -2,22 +2,10 @@ package ru.geekbrains.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import ru.geekbrains.entities.Task;
-
-import java.util.List;
 
 @Repository
 public interface TasksRepository extends JpaRepository<Task, Long>, JpaSpecificationExecutor<Task> {
 
-    @Query("select t from Task t where t.manager_id = :manager_id order by id")
-    List<Task> findByManager_id(@Param("manager_id") Long id);
-
-    @Query("select t from Task t where t.employer_id = :employer_id order by id")
-    List<Task> findByEmployer_id(@Param("employer_id") Long id);
-
-    @Query("select t from Task t where t.manager_id = :user_id or t.employer_id = :user_id order by id")
-    List<Task> findByManager_idAndEmployer_id(@Param("user_id") Long id);
 }
