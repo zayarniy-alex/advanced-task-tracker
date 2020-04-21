291b72f77daa32ccb6329fa75eec38e69b13101c
enjsite <enjyweb@gmail.com>
2020-04-21 16:30:59 +0300

    errors page and controller

---
+++ src/main/java/ru/geekbrains/controllers/TaskErrorController.java
@@ -0,0 +1,27 @@
+package ru.geekbrains.controllers;
+
+import org.springframework.boot.web.servlet.error.ErrorAttributes;
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.DispatcherServlet;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Controller
+public class TaskErrorController implements ErrorController {
+
+    @RequestMapping("/error")
+    public String handleError(HttpServletResponse response, Model model) {
+        model.addAttribute("error", response.getStatus());
+        return "error";
+    }
+
+    @Override
+    public String getErrorPath() {
+        return "/error";
+    }
+}
