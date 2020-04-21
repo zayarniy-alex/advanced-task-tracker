841c715ddfa615c696ebc2dae33fcbfd2e79e104
enjsite <enjyweb@gmail.com>
2020-04-21 17:19:45 +0300

    fix imports in TasksRepository

--- src/main/java/ru/geekbrains/repositories/TasksRepository.java
+++ src/main/java/ru/geekbrains/repositories/TasksRepository.java
@@ -2,6 +2,8 @@ package ru.geekbrains.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import ru.geekbrains.entities.Task;
 
