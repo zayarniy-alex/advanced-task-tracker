41948a332fba67f46ba93bc5eb4ee179dfb2b4da
enjsite <enjyweb@gmail.com>
2020-04-21 16:31:38 +0300

    deleting some imports

--- src/main/java/ru/geekbrains/controllers/TaskErrorController.java
+++ src/main/java/ru/geekbrains/controllers/TaskErrorController.java
@@ -1,14 +1,9 @@
 package ru.geekbrains.controllers;
 
-import org.springframework.boot.web.servlet.error.ErrorAttributes;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.DispatcherServlet;
-
-import javax.servlet.RequestDispatcher;
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @Controller
